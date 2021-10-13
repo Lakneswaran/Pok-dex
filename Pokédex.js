8077af890a9c739ff886f2924909329ff4c28923
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2021-10-13 16:57:48 +0200

    html added and js modified
    Header ready and working on fetch js

--- Pokédex.js
+++ Pokédex.js
@@ -1,1 +1,25 @@
-js
+
+(() => { 
+    
+    document.getElementById("hero-id").addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            document.getElementById("myFormID").submit();
+            return false;
+        }
+    });
+
+
+async function getApi(){
+
+
+        let data = await fetch("https://pokeapi.co/api/v2/");
+        let main = await data.json();
+        var heroID = parseInt(document.getElementById('hero-id').value);
+        var target = document.getElementById('target');
+
+            console.table(main)
+            
+
+        }   
+   getApi();
+})()
