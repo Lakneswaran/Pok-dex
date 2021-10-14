d155683ffcffd4d607a352ee5cbb203272efbed9
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2021-10-14 19:26:04 +0200

    Html, js and css
    fetcch working. ID, name and image working.

--- Pokédex.js
+++ Pokédex.js
@@ -1,25 +1,43 @@
 
 (() => { 
     
-    document.getElementById("hero-id").addEventListener("keyup", function(event) {
-        if (event.keyCode === 13) {
-            document.getElementById("myFormID").submit();
-            return false;
-        }
+     
+    var heroid = 1;
+    id = document.getElementById("poke-id");
+    id.addEventListener("keyup", (event)=>{
+        
+    if (event.keyCode === 13) {
+        var id = document.querySelector("#poke-id").value;
+        heroid = id;
+        getApi(heroid);
+        } 
     });
 
 
-async function getApi(){
+    async function getApi(a){
 
+        var pokeimg = document.getElementById("choosenoneimg");
+        var nameid = document.getElementById("header");
 
-        let data = await fetch("https://pokeapi.co/api/v2/");
+        var id = a;   
+        let data = await fetch("https://pokeapi.co/api/v2/pokemon/"+id+ "/");
         let main = await data.json();
-        var heroID = parseInt(document.getElementById('hero-id').value);
-        var target = document.getElementById('target');
+        
+        pokeimg.src= main["sprites"]["front_default"];
+        nameid.innerHTML = " "+main["id"]+" : "+main["name"];
+        
+        console.table(main);
+       
+        // var target= document.getElementById(target);
+        // target.setAttribute('img', "pokemon");
+    
+    }
 
-            console.table(main)
-            
-
-        }   
-   getApi();
+    getApi(heroid);
+  
+   
 })()
+
+
+
+// console.log(event.keyCode === 13 ? console.log('yes') : console.log("no"));
